5bb4e6c99379a0ceb615217ed40f801665d7c541
DESKTOP-NFH9TU7\sahabat <yuyus.bahtiarg@gmail.com>
2018-10-02 09:20:25 +0700

    barang

--- application/modules/barangs/views/barang_form.php
+++ application/modules/barangs/views/barang_form.php
@@ -29,138 +29,23 @@
                                 <small class="form-control-feedback"> Isi harga </small> 
                             </div>
                         </div>
-                    <!--/span-->
-                    </div>
-                    <!--/row-->
-
-                    <div class="row" hidden>
-                        <div class="col-md-6">
-                            <div class="form-group has-success">
-                                <label class="control-label">Gender</label>
-                                <select class="form-control custom-select" name="gender">
-                                    <option value="pria">Male</option>
-                                    <option value="perempuan">Female</option>
-                                </select>
-                                <small class="form-control-feedback"> Select your gender </small> 
-                            </div>
-                        </div>
-                        <!--/span-->
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label class="control-label">Tanggal Lahir</label>
-                                <input type="date" class="form-control" name="lahir" placeholder="dd/mm/yyyy" value="<?php echo (isset($user[0])) ? $user[0]['nama'] : ''; ?>">
-                                <small class="form-control-feedback"> </small> 
+                                <label class="control-label">Insentif</label>
+                                <input type="text" id="insentif" name="insentif" class="form-control" value="<?php echo (isset($barang[0])) ? $barang[0]['insentif'] : ''; ?>" required>
+                                <small class="form-control-feedback"> Isi insentif </small> 
                             </div>
                         </div>
-                        <!--/span-->
-                    </div>
-                    <!--/row-->
-                    <div class="row p-t-20">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label class="control-label">Password</label>
-                                <small class="form-control-feedback"><?php echo form_error('password', '<div class="alert alert-danger" >', '</div>'); ?> </small> 
-                                <input type="password" id="password" name="password" class="form-control" value="<?php echo (isset($user[0]['password'])) ? $user[0]['password'] : ''; ?>" required>
-                                <small class="form-control-feedback"> password lebih dari 6 karaker</small> 
-                            </div>
-                        </div>
-                        <!--/span-->
-                        <div class="col-md-6">
-                            <div class="form-group has-danger">
-                                <label class="control-label">Konfirmasi Password</label>
-                                <small class="form-control-feedback"><?php echo form_error('konfirmasi_password', '<div class="alert alert-danger" >', '</div>'); ?> </small> 
-                                <input type="password" id="konfirmasi_password" name="konfirmasi_password" class="form-control form-control-danger" value="<?php echo (isset($user[0]['password'])) ? $user[0]['password'] : ''; ?>" required>
-                                <small class="form-control-feedback"> tulis ulang password <?php echo form_error('field name', '<div class="error">', '</div>'); ?> </small>
-                                
-                            </div>
-                        </div>
-                    <!--/span-->
-                    </div>
-                    <div class="row p-t-20">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label class="control-label">No KTP</label>
-                                <small class="form-control-feedback"><?php echo form_error('no_ktp', '<div class="alert alert-danger" >', '</div>'); ?> </small> 
-                                <input type="number" id="firstName" name="no_ktp" required class="form-control" value="<?php echo (isset($user[0])) ? $user[0]['no_ktp'] : ''; ?>">
-                            </div>
-                        </div>
-                        <!--/span-->
-                        <div class="col-md-6">
-                            <div class="form-group has-danger">
-                                <label class="control-label">No Handphone</label>
-                                <small class="form-control-feedback"><?php echo form_error('no_hp', '<div class="alert alert-danger" >', '</div>'); ?> </small> 
-                                <input type="number" id="no_hp" name="no_hp" required class="form-control form-control-danger" value="<?php echo (isset($user[0])) ? $user[0]['handphone'] : ''; ?>">
-                                <small class="form-control-feedback"> This field has error. </small> 
+                                <label class="control-label">Detail</label>
+                                <input type="text" id="detail" name="detail" class="form-control" value="<?php echo (isset($barang[0])) ? $barang[0]['detail'] : ''; ?>" required>
+                                <small class="form-control-feedback"> Isi detail </small> 
                             </div>
                         </div>
                     <!--/span-->
                     </div>
                     <!--/row-->
-                    <div class="row p-t-20">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label class="control-label">Level</label>
-                                <select class="form-control custom-select" name="level" required>
-                                    <option value="admin" <?php echo (isset($user[0]) && $user[0]['level'] == 'admin') ? 'selected' : ''; ?> >Administrator</option>
-                                    <option value="owner" <?php echo (isset($user[0]) && $user[0]['level'] == 'owner') ? 'selected' : ''; ?>>Owner</option>
-                                    <option value="member" <?php echo (isset($user[0]) && $user[0]['level'] == 'member') ? 'selected' : ''; ?>>Pelanggan</option>
-                                </select>
-                            </div>
-                        </div>
-                        <!--/span-->
-                       
-                    </div>
-                    <!--/row-->
-
-                    <h3 class="box-title m-t-40">Address</h3>
-                    <hr>
-                    <div class="row">
-                        <div class="col-md-12 ">
-                            <div class="form-group">
-                                <label>Street</label>
-                                <input type="text" class="form-control" name="alamat" required value="<?php echo (isset($user[0])) ? $user[0]['alamat'] : ''; ?>">
-                                <small class="form-control-feedback"> isi alamat sesuai KTP.</small>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row" hidden>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>City</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <!--/span-->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>State</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <!--/span-->
-                    </div>
-                    <!--/row-->
-                    <div class="row" hidden>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>Post Code</label>
-                                <input type="text" class="form-control">
-                            </div>
-                        </div>
-                        <!--/span-->
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>Country</label>
-                                <select class="form-control custom-select">
-                                    <option>--Select your Country--</option>
-                                    <option>India</option>
-                                    <option>Sri Lanka</option>
-                                    <option>USA</option>
-                                </select>
-                            </div>
-                        </div>
-                        <!--/span-->
-                    </div>
                 </div>
                 <div class="form-actions">
                     <button type="submit" class="btn btn-success" name="submit"> <i class="fa fa-check"></i> Simpan</button>
