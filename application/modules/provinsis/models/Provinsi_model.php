383867d01f58bc542c4845550b9ce99eb2a68ecc
DESKTOP-NFH9TU7\sahabat <yuyus.bahtiarg@gmail.com>
2018-11-27 21:32:38 +0700

    dsas

--- application/modules/provinsis/models/Provinsi_model.php
+++ application/modules/provinsis/models/Provinsi_model.php
@@ -33,12 +33,12 @@ class Provinsi_model extends CI_Model
 	function get_all_provinsi() { //datatable server side, colom sesuaikan dgn tabel jgn di lebihi id saja
       $this->datatables->from('provinsis p');
       //select kolom yang dimunculkan di tabel saja, bila tidak search|sort tidak akan berjalan
-      $this->datatables->select('nama, user_id');
+      $this->datatables->select('provinsi_nama, provinsi_id');
       $this->datatables->add_column('action', 
       		'<a href="'.base_url().'provinsis/ubah/$1" class="btn btn-primary btn-flat btn-addon btn-sm m-b-10 m-l-5" title="Lihat Data"><i class="fa fa-eye"></i></a>
       		<a class="edit-link btn btn-warning btn-flat btn-addon btn-sm m-b-10 m-l-5" title="Edit Data" href="'.base_url().'provinsis/ubah/$1"><i class="fa fa-edit"></i></a>
       		<a class="hapus-link btn btn-danger btn-flat btn-addon btn-sm m-b-10 m-l-5" title="Hapus Data" onClick=del("$1")><i class="fa fa-trash"></i></a>'
-      	, 'user_id');
+      	, 'provinsi_id');
       return $this->datatables->generate();
   }
 }
