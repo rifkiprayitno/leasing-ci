2863034ba239e57db76b2c6fdbce4441ee82c13e
rifkiprayitno <ahmadrifkiprayitno@gmail.com>
2019-02-19 13:52:44 +0700

    merge

---
+++ application/helpers/log_helper.php
@@ -0,0 +1,33 @@
+<?php
+function helper_log($tipe = "", $str = ""){
+    $CI =& get_instance();
+ 
+    if (strtolower($tipe) == "login"){
+        $log_tipe   = 0;
+    }
+    elseif(strtolower($tipe) == "logout")
+    {
+        $log_tipe   = 1;
+    }
+    elseif(strtolower($tipe) == "add"){
+        $log_tipe   = 2;
+    }
+    elseif(strtolower($tipe) == "edit"){
+        $log_tipe  = 3;
+    }
+    else{
+        $log_tipe  = 4;
+    }
+ 
+    // paramter
+    $param['log_user']      = $CI->session->userdata('ses_id');
+    $param['log_tipe']      = $log_tipe;
+    $param['log_desc']      = $str;
+ 
+    //load model log
+    $CI->load->model('m_log');
+ 
+    //save to database
+    $CI->m_log->save_log($param);
+ 
+}
